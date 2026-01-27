3d0e26cd50585f3f2f83fef6633b2e56aff9c298
gjulia <julia.gari75@ynov.com>
2026-01-27 16:42:24 +0100

    Remove old meteo-trip folder

--- meteo-trip/database_migration.sql
+++
@@ -1,7 +0,0 @@
--- Add first_name and last_name columns to itineraries table
-ALTER TABLE itineraries 
-ADD COLUMN IF NOT EXISTS first_name TEXT,
-ADD COLUMN IF NOT EXISTS last_name TEXT;
-
--- Note: The username is stored in auth.users metadata, no table change needed
--- You can access it via user.user_metadata.username in your code
